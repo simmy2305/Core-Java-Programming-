de2b29691d26288479e8766338379385c9af69cb
simmy2305 <simranbajiya5@gmail.com>
2025-01-07 15:09:47 +0530

    addition in 2D array

---
+++ h/Additionin2Darray.java
@@ -0,0 +1,18 @@
+public class Additionin2Darray {
+    public static void main(String[] args) {
+        int a[][]={{1,2,3},{4,5,6}};
+        int b[][]={{1,2,3},{4,5,6}};
+        int sum[][]=new int[2][3];
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a[i].length;j++){
+                sum[i][j] = a[i][j]+b[i][j];
+            }
+        }
+        for(int i=0;i<sum.length;i++){
+            for(int j=0;j<sum[i].length;j++){
+             System.out.print(sum[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
