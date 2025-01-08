90449e86634d5c2e185c48259a291781c6450b6b
simmy2305 <simranbajiya5@gmail.com>
2025-01-08 14:55:42 +0530

    sum of  all element of java

---
+++ h/Sumof2Darray.java
@@ -0,0 +1,12 @@
+public class Sumof2Darray {
+    public static void main(String[] args) {
+        int a[][]={{1,2,3},{4,5,6},{7,8,9}};
+        int sum=0;
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a[i].length;j++){
+             sum=sum+a[i][j];
+            }
+        }
+        System.out.println("sum of all element of java is : "+sum);
+    }
+}
