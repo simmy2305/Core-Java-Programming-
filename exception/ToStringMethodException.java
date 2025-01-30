7a775e7e74be31829699d6ea260a6c5beb3a41fa
simmy2305 <simranbajiya5@gmail.com>
2025-01-30 11:38:00 +0530

    tostring method in exception

---
+++ exception/ToStringMethodException.java
@@ -0,0 +1,10 @@
+public class ToStringMethodException {
+    public static void main(String[] args) {
+        try {
+            int a=9/0;
+            System.out.println(a);
+        } catch (ArithmeticException ex) {
+           System.out.println(ex.toString());
+        }
+    }
+}
