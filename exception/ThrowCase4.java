ba38499b20707bc2c9d90de054f4ed378e9d4df8
simmy2305 <simranbajiya5@gmail.com>
2025-01-30 16:52:28 +0530

    throw exception case four in java

---
+++ exception/ThrowCase4.java
@@ -0,0 +1,6 @@
+public class ThrowCase4 {
+    public static void main(String[] args) {
+        throw new ThrowCase4();
+        //error:bcz we Throwcase4 is a normal class not an exception class
+    }
+}
